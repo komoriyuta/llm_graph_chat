e5007b63d09c12105b0374045f8fcf7df90fc68b
komoriyuta <komoriyuta@users.noreply.github.com>
2025-09-20 04:43:48 +0000

    deploy: 8a7aa6dac1f68c98cc3d12bab9270c9c98e0eb80

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1903840358"
+    serviceWorkerVersion: "1725490409"
   }
 });
